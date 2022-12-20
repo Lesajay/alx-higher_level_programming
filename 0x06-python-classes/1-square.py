d54b94ad0f504aad7c020d4acdafebedc8280d5d
Lesajay <craigmazwi@gmail.com>
2022-12-20 16:26:05 +0000

    all

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+a class Square that defines a square by: (based on 1-square.py)
+"""
+
+
+class Square:
+    """Class Square that has attributes. Instantiation with size
+    Attributes:
+        size (int): The size of the square
+    """
+
+    def __init__(self, size):
+        """The __init__ method for Square class
+        Args:
+            size: (:obj: 'int'): A private instance size
+        """
+        self.__size = size
