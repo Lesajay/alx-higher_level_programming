4d23cccf46f4de87e9a7cf871627f6c87674592a
Lesajay <craigmazwi@gmail.com>
2023-01-02 18:37:01 +0000

    first

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/pyhton3
+# -*- coding: utf-8 -*-
+"""
+an empty class Rectangle that defines a reactangle
+"""
+
+
+class Rectangle:
+    """Empty class Square that defines a square
+    Attributes:
+        empty
+   """
+    pass
