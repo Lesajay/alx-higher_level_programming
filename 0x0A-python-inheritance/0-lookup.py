ca98df873e232e528e28a1f8d49229378be1e249
Lesajay <craigmazwi@gmail.com>
2023-01-09 09:10:40 +0000

    first

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Module that contains afunction that returns all objects 
+
+Lesajay
+"""
+
+
+def lookup(obj):
+    """
+    Returns all objects in an objects dictionary as a list
+    """
+    return dir(obj)
