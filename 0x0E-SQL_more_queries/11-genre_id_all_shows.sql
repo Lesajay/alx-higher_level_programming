7f4a71c31d82e0a5e3fe3a10f3ff95b1df87e203
Lesajay <craigmazwi@gmail.com>
2023-02-18 15:48:14 +0000

    corrections

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
-ll shows in database
+--ll shows in database
 --  lists all shows contained in the database hbtn_0d_tvshows. If a show doesnt have a genre, display NULL
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows LEFT JOIN tv_show_genres
